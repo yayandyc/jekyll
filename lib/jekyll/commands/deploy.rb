904ea4606ab049267e672bf36466fe5300450215
Andy Connaughton <andy@yappy.com.au>
2016-04-05 19:01:57 +0800

    Cleaned up a little

--- lib/jekyll/commands/deploy.rb
+++ lib/jekyll/commands/deploy.rb
@@ -15,7 +15,7 @@ module Jekyll
             c.option 'deploy_to', '--deploy_to DEPLOY_TO', String, 'Deploy to a particular configuration'
 
             c.action do |_, options|
-              #Jekyll::Commands::Build.process(options)
+              Jekyll::Commands::Build.process(options)
               Jekyll::Commands::Deploy.process(options)
             end
           end
@@ -30,18 +30,20 @@ module Jekyll
                     
           case deploy_bits[0].downcase
           when 's3'
+            # Initialize the S3 Resource
             s3_resource = initializeS3(options)
             
+            # Work out the bucket and the prefix from the endpoint
             bucket_bits = deploy_bits[1].split('/')
             bucket_name = bucket_bits.shift
             prefix = bucket_bits.join('/')
             
+            # Prepare what changes need to be applied
             local_objects = prepare_local(options['destination'], '')
-            
             object_actions = prepare_actions(s3_resource, bucket_name, prefix, local_objects)
             
+            # Do the changes
             deploy_to_s3(s3_resource, bucket_name, options['destination'], object_actions)
-            
           else
             Jekyll.logger.error("Unknown deployment location '" + options['deploy_to'] + "'")
             exit(1)
@@ -133,78 +135,8 @@ module Jekyll
             Jekyll.logger.error("There is no known AWS credentials for S3 deployment!")
             exit(1)
           end
-          
-          
         end
         
-        def deploy_to_aws_s3(dest, s3_client, bucket, prefix)
-          
-          Dir.foreach(dest) do |fname|
-            next if fname == '.' || fname == '..'
-            
-            next_object = prefix.empty? ? fname : [prefix, fname].join('/')
-            disk_path = "#{dest}/#{fname}"
-             
-            if File.directory?(disk_path)
-              deploy_to_aws_s3(disk_path, s3_client, bucket, next_object)
-            else
-              do_object(disk_path, next_object, s3_client, bucket)
-            end
-          end
-        end
-        
-#        def get_bucket_objects(bucket_name, prefix, s3_client)
-#          config = s3_client.config
-#          bucket = Aws::S3::Resource.new(
-#            access_key_id: config['access_key_id'],
-#            secret_access_key: config['secret_access_key'],
-#            region: config['region'])
-#          ret = Array.new
-#          
-#          bucket.objects({prefix: prefix}).each do |obj|
-#            ret.push(obj.key)
-#            #puts "  #{obj.key} => #{obj.etag}"
-#          end
-#          ret
-#        end
-        
-        def do_object(disk_path, object_path, s3_client, bucket)      
-          file_md5 = Digest::MD5.hexdigest(File.read(disk_path))
-          push_required = true
-          
-          print "#{bucket}::#{object_path} - "
-          
-          s3_client.bucket(bucket).objects.each do |objectsummary|
-            if(objectsummary.key == object_path)
-              puts " --- " + objectsummary.key
-            end
-          end
-          
-#          s3_client.bucket(bucket).objects.each do |objectsummary|
-#            if(objectsummary.key == object_path)
-#              resp = s3_client.head_object({
-#                bucket: bucket,
-#                key: "#{object_path}"
-#              })
-#              
-#              if !!resp.metadata['object_hash']
-#                push_required = resp.metadata['object_hash'] != file_md5
-#              end
-#            end
-#          end
-#             
-#          if push_required
-#            puts "Deploying new file."
-#            File.open(disk_path, 'rb') do |file|
-#              s3_client.put_object(bucket: bucket, key: object_path.split('/').join('/'), body: file,
-#              metadata: {object_hash: file_md5})
-#            end
-#          else
-#            puts "No change"
-#          end
-          
-          
-        end
       end
     end
   end
